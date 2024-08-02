eb45d3a46fc38ab09f56966ffdd921ea94cbbd9c
pouyan alizadeh <pouyan9913@gmail.com>
2024-08-02 19:57:35 +0330

    Add project files.

---
+++ Domain/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using Pouyan.Tools.Extensions;
+
+namespace Domain
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public Guid? ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
---
+++ Domain/Entities/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Post : BaseEntity
+    {
+        public required string Title { get; set; }
+        public string? Content { get; set; }
+        public Guid? CategoryId { get; set; }
+
+    }
+}
