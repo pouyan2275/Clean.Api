7d7cdee64e5f5a530a8d00418d362bc6b4295ce3
pouyan alizadeh <pouyan9913@gmail.com>
2024-08-09 23:04:52 +0330

    irepository

---
+++ Domain/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IRepository<TEntity,TId>
+    {
+        Task<TEntity> GetById(TId id);
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> Create(TEntity Tentity);
+        Task<TEntity> Update(TId id, TEntity Tentity);
+        Task<Guid> Delete(TId id);
+    }
+}
