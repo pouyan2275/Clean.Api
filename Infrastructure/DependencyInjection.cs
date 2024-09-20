df7a9376f9ccdda4fa64acdb82564bffb92b5c31
pouyan alizadeh <pouyan9913@gmail.com>
2024-09-20 10:44:59 +0330

    add dependency injection for each project

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Interfaces.Repositories;
+using Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>();
+
+        return services;
+    }
+}
