137752571f7a17882c8c108719b9e5e41b1cb77e
pouyan alizadeh <pouyan9913@gmail.com>
2024-09-21 23:11:14 +0330

    add di

--- Clean.Api/Program.cs
+++ Clean.Api/Program.cs
@@ -1,23 +1,19 @@
-﻿using Application.Bases;
+﻿using Api.Bases;
+using Application.Bases;
 using Infrastructure.Bases;
 using Serilog;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services
     .AddApplication()
-    .AddInfrastructure();
+    .AddInfrastructure()
+    .AddPresentation();
 
 builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
 
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
